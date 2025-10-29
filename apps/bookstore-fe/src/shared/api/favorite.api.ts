d9ce908b8f50bf3e3157f145ab47803ff97ac78d
dave <dave@daves-Mac-mini.local>
2025-10-29 16:30:50 -0700

    feat(api): missing favorite type

--- apps/bookstore-fe/src/shared/api/favorite.api.ts
+++ apps/bookstore-fe/src/shared/api/favorite.api.ts
@@ -1,6 +1,6 @@
-import { Favorite } from '@favoritestore/types/lib/favorite.type';
+import { Favorite } from '@bookstore/types/lib/favorite.type';
 import axiosInstance from './axios-instance';
 
-export const getFavorites = () : favorite => axiosInstance.get('/favorites');
+export const getFavorites = () : Favorite => axiosInstance.get('/favorites');
 export const addFavorite = (id: string) => axiosInstance.post(`/favorites`);
 export const removeFavorite = (id: string) => axiosInstance.delete(`/favorites/${id}`);
